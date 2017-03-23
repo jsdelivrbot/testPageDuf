f61a9bd4041ab948376fe46552a0e1a27a7ee3c0
mzuber <michallzuber@gmail.com>
2017-03-23 09:40:10 +0100

    Add bootstrap v4.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import IntroComponent from './intro';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <IntroComponent/>
     );
   }
 }
